6da328abffb0fd1bc562b497249da78c74fb546c
ivyytheone <jamie.bech1997@gmail.com>
2024-08-30 09:19:25 +0200

    Initial commit

---
+++ Github-Backups-Bash.sh
@@ -0,0 +1,25 @@
+##########################################
+######## Jamie Bech 2024-08-30 ###########
+######### jamie.bech@bth.com #############
+########    Github-Backups     ###########
+##########################################
+#Scriptet tar en backup på Github Repo och sparar i satt directory
+
+#!/bin/bash
+
+# GitHub repository information
+OWNER="REPO_OWNER"
+REPO="REPO_NAME"
+BACKUP_DIR="backups"
+
+# Create backup directory if it doesn't exist
+mkdir -p $BACKUP_DIR
+
+# Clone the repository
+gh repo clone $OWNER/$REPO $BACKUP_DIR/$REPO
+
+# Compress the backup
+tar -czf $BACKUP_DIR/$REPO.tar.gz -C $BACKUP_DIR $REPO
+
+# Remove the cloned repository
+rm -rf $BACKUP_DIR/$REPO
---
+++ Github-backups-Python.py
@@ -0,0 +1,35 @@
+##########################################
+######## Jamie Bech 2024-08-30 ###########
+######### jamie.bech@bth.com #############
+########    Github-Backups     ###########
+##########################################
+#Scriptet tar en backup på Github Repo och sparar i satt directory
+
+import requests
+import os
+
+# GitHub API token
+token = 'YOUR_GITHUB_TOKEN'
+headers = {'Authorization': f'token {token}'}
+
+# Repository information
+owner = 'REPO_OWNER'
+repo = 'REPO_NAME'
+
+# Directory to save backups
+backup_dir = 'backups'
+
+# Create backup directory if it doesn't exist
+os.makedirs(backup_dir, exist_ok=True)
+
+# Fetch and save pull requests
+pulls_url = f'https://api.github.com/repos/{owner}/{repo}/pulls'
+pulls = requests.get(pulls_url, headers=headers).json()
+with open(os.path.join(backup_dir, 'pull_requests.json'), 'w') as f:
+    json.dump(pulls, f)
+
+# Fetch and save commits
+commits_url = f'https://api.github.com/repos/{owner}/{repo}/commits'
+commits = requests.get(commits_url, headers=headers).json()
+with open(os.path.join(backup_dir, 'commits.json'), 'w') as f:
+    json.dump(commits, f)
