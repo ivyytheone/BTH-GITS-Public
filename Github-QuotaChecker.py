c4b7bbe5eb04723172eaa6100809b169acbb3157
ivyytheone <jamie.bech1997@gmail.com>
2024-08-30 09:24:41 +0200

    Adding Quota checkers
    Partly written by Copilot

---
+++ Github-QuotaChecker.py
@@ -0,0 +1,25 @@
+##########################################
+######## Jamie Bech 2024-08-30 ###########
+######### jamie.bech@bth.com #############
+########    Git-QuotaMonitor   ###########
+##########################################
+#Scriptet checkar Quota i repos
+
+import requests
+
+# GitHub API token
+token = 'YOUR_GITHUB_TOKEN'
+headers = {'Authorization': f'token {token}'}
+
+# Repository information
+owner = 'REPO_OWNER'
+repo = 'REPO_NAME'
+
+# Fetch repository details
+repo_url = f'https://api.github.com/repos/{owner}/{repo}'
+response = requests.get(repo_url, headers=headers)
+repo_data = response.json()
+
+# Print repository size in MB
+repo_size = repo_data['size'] / 1024
+print(f'Repository size: {repo_size} MB')
