4e78e743134e11f52f7df0dc377647a83138d7ca
Jamie Bech <jamie.bech1997@gmail.com>
2024-09-27 08:23:02 +0200

    CredentialPopup and Versionstring for multiple versions
    +Credential window popup
    +Versionstring can be multiple versions

--- Powershell/RemoveOldTeamsKeysV2.ps1
+++ Powershell/RemoveOldTeamsKeysV2.ps1
@@ -7,13 +7,16 @@
 # Searchpath to txt file with computer names and logfile.
 $computerListFile = "C:\temp\TeamsKeys\computers.txt"
 $logFile = "C:\temp\TeamsKeys\LogFile.txt"
-$versionToRemove = "1.2.0.24753"
+$versionsToRemove = @("1.2.0.24753")
 
 # Read computernames from txt
 $computers = Get-Content -Path $computerListFile
 
+# Prompt for credentials
+$credential = Get-Credential
+
 $scriptBlock = {
-    param ($versionToRemove, $logFile)
+    param ($versionsToRemove, $logFile)
     $registryPaths = @(
         "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Teams",
         "HKU:\*\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Teams"
@@ -24,10 +27,10 @@ $scriptBlock = {
         $subKeys = Get-ChildItem -Path $registryPath -ErrorAction SilentlyContinue
         foreach ($subKey in $subKeys) {
             $version = (Get-ItemProperty -Path $subKey.PSPath).DisplayVersion
-            if ($version -eq $versionToRemove) {
+            if ($versionsToRemove -contains $version) {
                 Remove-Item -Path $subKey.PSPath -Recurse
                 $found = $true
-                $logMessage = "$(Get-Date) - $env:COMPUTERNAME - Tog bort Teams version $versionToRemove frÃ¥n registret."
+                $logMessage = "$(Get-Date) - $env:COMPUTERNAME - Tog bort Teams version $version från registret."
                 Add-Content -Path $logFile -Value $logMessage
                 Write-Output $logMessage
             }
@@ -35,7 +38,7 @@ $scriptBlock = {
     }
 
     if (-not $found) {
-        $logMessage = "$(Get-Date) - $env:COMPUTERNAME - Ingen Teams version $versionToRemove hittades."
+        $logMessage = "$(Get-Date) - $env:COMPUTERNAME - Ingen Teams version hittades som matchar någon av de angivna versionerna."
         Add-Content -Path $logFile -Value $logMessage
         Write-Output $logMessage
     }
@@ -44,14 +47,14 @@ $scriptBlock = {
 foreach ($computer in $computers) {
     # Ping the computer to check if it's reachable
     if (Test-Connection -ComputerName $computer -Count 2 -Quiet) {
-        $logMessage = "$(Get-Date) - $computer - Datorn kan nÃ¥s via ping."
+        $logMessage = "$(Get-Date) - $computer - Datorn kan nås via ping."
         Add-Content -Path $logFile -Value $logMessage
         Write-Output $logMessage
-        Invoke-Command -ComputerName $computer -ScriptBlock $scriptBlock -ArgumentList $versionToRemove, $logFile
+        Invoke-Command -ComputerName $computer -ScriptBlock $scriptBlock -ArgumentList $versionsToRemove, $logFile -Credential $credential
     }
     else {
         # Log the failure to reach the computer
-        $logMessage = "$(Get-Date) - $computer - Kunde inte nÃ¥ datorn via ping."
+        $logMessage = "$(Get-Date) - $computer - Kunde inte nå datorn via ping."
         Add-Content -Path $logFile -Value $logMessage
         Write-Output $logMessage
     }
